99e87d9ec7f5b471fd97edbfd65d911d03e060bc
MicroTransactionsMatterToo <ennisbaradine@gmail.com>
2017-03-24 22:40:25 +1300

    Added some copyrights/credits and added .gitignore

--- WINTYPES.h
+++ WINTYPES.h
@@ -1,9 +1,22 @@
+// Windows Types for NON-Windows platforms. Types are based off of the Windows 10 SDK, and while they work on Windows, you should use the actual SDK 
+/*
+Copyright 2017 Ennis Massey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+// Credit for basic types goes to the people at Microsoft who trawl through these stupidly large headers
+
 #if !defined(_WIN32_WINNT) && !defined(_CHICAGO_)
 #define  _WIN32_WINNT   0x0603
 #endif
 #ifndef WINVER
 #define WINVER _WIN32_WINNT
 #endif
+
 
 #ifdef STRICT
 typedef void *HANDLE;
@@ -17,6 +30,7 @@ typedef PVOID HANDLE;
 #define DECLARE_HANDLE(name) typedef HANDLE name;
 #endif
 typedef HANDLE *PHANDLE;
+
 
 
 #if __MACH__ && __APPLE__ || __gnu_linux__
